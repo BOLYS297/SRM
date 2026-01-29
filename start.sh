ac646ac8f8cae317f4757bc9f24e4cadd55ce2d6
BOLYS297 <dd9159360@gmail.com>
2026-01-29 22:40:25 +0100

    Fix start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -e
 
-php artisan key:generate --force
+if [ -z "$APP_KEY" ]; then
+  echo "APP_KEY manquant. Ajoute APP_KEY dans Render > Environment."
+  exit 1
+fi
+
 php artisan package:discover --ansi
 php artisan config:cache
 php artisan migrate --force
